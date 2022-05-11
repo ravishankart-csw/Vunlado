b0779453e1fa51afa1e33d6007c67aff26dbb52c
ravishankart-csw <70476420+ravishankart-csw@users.noreply.github.com>
2022-05-11 20:21:30 +0530

    Update CowController.java

--- src/main/java/com/scalesec/vulnado/CowController.java
+++ src/main/java/com/scalesec/vulnado/CowController.java
@@ -11,6 +11,5 @@ public class CowController {
     @RequestMapping(value = "/cowsay")
     String cowsay(@RequestParam(defaultValue = "I love Linux!") String input) {
         return Cowsay.run(input);
-        //return Cowsay.run(input);
-    }
+            }
 }
